9373c2457f89a888b388190059765a7f3d62bcd7
Gebxby <gabrielfenanilmupengetahuan@gmail.com>
2025-08-30 18:53:12 +0700

    fix video bug #5

---
+++ app/api/upload/route.ts
@@ -0,0 +1,19 @@
+import { put } from '@vercel/blob';
+ 
+export async function POST(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const filename = searchParams.get('video/anniv.mp4');
+  if (!filename) {
+    return new Response('Missing "anniv.mp4" query parameter', { status: 400 });
+  }
+ 
+  if (!request.body) {
+    return new Response('Missing request body', { status: 400 });
+  }
+  const blob = await put(filename, request.body, {
+    access: 'public',
+    addRandomSuffix: true
+  });
+ 
+  return Response.json(blob);
+}
